0bf5a9dcd4afb89bed5fedfd3e6c076763abc7c5
wangyanchen <wenjuyanchen@foxmail.com>
2020-09-26 17:01:49 +0800

    添加schemudled

--- src/main/java/com/chen/xia/XiaApplication.java
+++ src/main/java/com/chen/xia/XiaApplication.java
@@ -3,9 +3,11 @@ package com.chen.xia;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @MapperScan("com.chen.xia.mapper")
 @SpringBootApplication
+@EnableScheduling
 public class XiaApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/chen/xia/timertask/scheduled/ScheduledUser.java
@@ -0,0 +1,35 @@
+package com.chen.xia.timertask.scheduled;
+
+import com.chen.xia.entity.User;
+import com.chen.xia.util.RedisUtil;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.Date;
+
+/**
+ * Scheduled 定时任务
+ */
+@Component
+public class ScheduledUser {
+
+    @Resource
+    private RedisUtil redisUtil;
+
+    /**
+     * 定时任务
+     * @Scheduled:  设置定时任务
+     * cron属性: cron表达式，定时任务触发时间
+     */
+    @Scheduled(cron = "0/2 * * * * *")
+    public void addUser(){
+        User user = (User) redisUtil.get("chen");
+        if (user!=null){
+            System.out.println("调用定时任务..."+new Date()+user.toString());
+        }else {
+            System.out.println("调用定时任务..."+new Date());
+        }
+
+    }
+}
